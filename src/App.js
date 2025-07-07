bca9ebde4faa0eaa7c87ecb951a30597b8843e11
trixiemeisye <trixiemeisye@gmail.com>
2025-07-08 02:50:42 +0700

    Perbaiki responsif gambar About dan setup deploy ke GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import About from './About';
 import Work from './Work';
